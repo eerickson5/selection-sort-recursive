b31bd35dde9c3512e0f39402ef2acf124c160010
Elise Erickson <elisece717@gmail.com>
2023-11-29 20:33:51 -0500

    this was fun

--- javascript/selection_sort_recursive.js
+++ javascript/selection_sort_recursive.js
@@ -1,11 +1,25 @@
 function selectionSortRecursive(arr) {
-  // type your code here
+  if(arr.length > 1){
+    const min = Math.min(...arr);
+    const index = arr.indexOf(min);
+    let editableArr = [...arr];
+    editableArr.splice(index, 1);
+    return([min, ...selectionSortRecursive(editableArr)])
+  } else {
+    return arr;
+  }
+  
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: [-1, 2, 3, 5]");
-  console.log("=>", selectionSortRecursive([3, -1, 5, 2]));
+  console.log("Expecting: [-1, 2, 3, 5, 234, 523]");
+  console.log("=>", selectionSortRecursive([3, -1, 5, 2, 234, 523]));
+
+  console.log("");
+
+  console.log("Expecting: [-1]");
+  console.log("=>", selectionSortRecursive([-1]));
 
   console.log("");
 }
@@ -14,3 +28,10 @@ module.exports = selectionSortRecursive;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+/*
+what i want to do here is 
+find the lowest
+return it and result of recursion(rest of array)
+it would be faster to modify the original array with splice but i'm supposed to make a copy so ill do that
+*/
